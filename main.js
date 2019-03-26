5dfa8b7c12adb3ed5224120e0887979b942d2ae9
Naokazu Terada <naokazu.terada@gmail.com>
2019-03-27 01:27:37 +0900

    Use setInterval

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
-console.log('extension:main.js');
-// Run code in the web page context
-var script = document.createElement('script');
-let code = document.createTextNode(`
-console.log('ex');
-// console.log('Extension',videojs.options.playbackRates);
-// videojs.options.playbackRates = [0.5, 0.75, 1, 2, 3, 4];
-// let popcorn = Popcorn('#podcast-audio-player_html5_api');
+// Replace playback rates
+let interval = setInterval(()=>{
+  let player = document.getElementById('podcast-audio-player')
+  if(player) {
+    player.setAttribute('data-setup', '{ "playbackRates": [0.7, 0.8, 0.9, 1], "controlBar": { "fullscreenToggle": false, "timeDivider": false, "download": true } }')
+    clearInterval(interval)
+  }
+}, 1)
 
-document.getElementById('podcast-audio-player').setAttribute('data-setup', '{ "playbackRates": [0.5, 0.75, 1], "controlBar": { "fullscreenToggle": false, "timeDivider": false, "download": true } }')
-// let popcorn = Popcorn('#podcast-audio-player_html5_api');
-// console.log('videojs',videojs);
-// console.log(videojs(document.getElementById('podcast-audio-player_html5_api')));
-`);
-console.log(document.readyState);
-script.appendChild(code);
-document.addEventListener('DOMContentLoaded', ()=> {
-  console.log('DOMContentLoaded');
-  // document.head.appendChild(script);
-  // var scr = document.querySelector('script[src="https://vjs.zencdn.net/6.0.1/video.js"]');
-  // console.log('scr.parent',scr.parentNode);
-  // scr.parentNode.removeChild(scr);
-  document.head.prepend(script);
-  // document.body.appendChild('<script type="text/javascript" src="https://www.heavybit.com/wp-content/themes/heavybit/assets/js/popcorn.min.js"></script>');
-  // document.body.appendChild('<script src="https://vjs.zencdn.net/6.0.1/video.js"></script>');
-});
-
-// Space key function replacement
+// Toggle play/pause with space key
 document.addEventListener('keypress', function(e) {
   if(e.keyCode==32){
     e.preventDefault()
