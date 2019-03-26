b5aba2a94eb77574c4309f57bb40024c87799f78
Naokazu Terada <naokazu.terada@gmail.com>
2019-03-27 01:51:11 +0900

    Change playback rates

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
 let interval = setInterval(()=>{
   let player = document.getElementById('podcast-audio-player')
   if(player) {
-    player.setAttribute('data-setup', '{ "playbackRates": [0.8, 0.9, 1], "controlBar": { "fullscreenToggle": false, "timeDivider": false, "download": true } }')
+    player.setAttribute('data-setup', '{ "playbackRates": [0.75, 0.85, 1], "controlBar": { "fullscreenToggle": false, "timeDivider": false, "download": true } }')
     clearInterval(interval)
   }
 }, 1)
