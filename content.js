4d78882b57dec416be331da5125a9217d1c04db6
Naokazu Terada <naokazu.terada@gmail.com>
2019-03-27 14:55:21 +0900

    Rewind and forward by arrows keys

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+const STEP_SECONDS = 7
+
 // Replace playback rates
 let interval = setInterval(()=>{
   let player = document.getElementById('podcast-audio-player')
@@ -5,14 +7,32 @@ let interval = setInterval(()=>{
     let attr = JSON.parse(player.getAttribute('data-setup'))
     attr.playbackRates = [0.7, 0.8, 1]
     player.setAttribute('data-setup', JSON.stringify(attr))
+    // Toggle play/pause with space key
+    document.addEventListener('keydown', function(e) {
+      if(e.keyCode==32) {
+        e.preventDefault()
+        document.querySelector('.vjs-play-control').click()
+      }
+    });
     clearInterval(interval)
   }
 }, 1)
 
-// Toggle play/pause with space key
-document.addEventListener('keypress', function(e) {
-  if(e.keyCode==32){
-    e.preventDefault()
-    document.querySelector('.vjs-play-control').click()
+// this.player_.currentTime(this.player_.currentTime() - STEP_SECONDS);
+
+let code = document.createTextNode(`
+document.addEventListener('keydown', function(e) {
+  let player = videojs.players['podcast-audio-player']
+  if(e.keyCode==37) {
+    player.currentTime(player.currentTime() - ${STEP_SECONDS})
   }
+  else if(e.keyCode==39) {
+    player.currentTime(player.currentTime() + ${STEP_SECONDS})
+  }
+});
+`);
+let script = document.createElement('script')
+script.appendChild(code)
+window.addEventListener('load', ()=> {
+  document.body.appendChild(script);
 });
