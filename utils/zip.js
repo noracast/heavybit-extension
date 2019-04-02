13a3e6b860836dffaf3e44806005cab81f71a7c8
Naokazu Terada <naokazu.terada@gmail.com>
2019-04-02 23:20:05 +0900

    Fix source

--- utils/zip.js
+++ utils/zip.js
@@ -1,6 +1,6 @@
 var zip = require('zip-dir')
 var package = require('../package.json')
 
-zip('src', { saveTo: `dist/${package.name}-${package.version}.zip` }, function (err, buffer) {
+zip('build', { saveTo: `dist/${package.name}-${package.version}.zip` }, function (err, buffer) {
   console.error(err)
 });
