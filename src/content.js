9ec838156dfdaf2e4af9d9def788ce1b2f1381b2
Naokazu Terada <naokazu.terada@gmail.com>
2019-04-02 23:07:45 +0900

    Cleanup

--- src/content.js
+++ src/content.js
@@ -1,18 +1,21 @@
 // Replace playback rates
-let interval = setInterval(()=>{
+let interval = setInterval(()=> {
   let player = document.getElementById('podcast-audio-player')
   if(player) {
+
+    clearInterval(interval)
+
+    // Toggle play/pause with space key
+    // ---------------------------------
+
     let attr = JSON.parse(player.getAttribute('data-setup'))
     attr.playbackRates = [0.7, 0.8, 1]
     player.setAttribute('data-setup', JSON.stringify(attr))
-    // Toggle play/pause with space key
-    document.addEventListener('keydown', function(e) {
+    document.addEventListener('keydown', (e)=> {
       if(e.keyCode==32) {
         e.preventDefault()
         document.querySelector('.vjs-play-control').click()
       }
-    });
-    clearInterval(interval)
   }
 }, 1)
 
@@ -21,4 +24,4 @@ let script = document.createElement('script')
 script.appendChild(code)
 window.addEventListener('load', ()=> {
   document.body.appendChild(script);
-});
+})
