d41e593d6dd7c6050e0e8bc18d16f77d2ed90b56
Naokazu Terada <naokazu.terada@gmail.com>
2019-04-04 22:16:42 +0900

    Fix comment

--- src/content.js
+++ src/content.js
@@ -5,12 +5,16 @@ let interval = setInterval(()=> {
 
     clearInterval(interval)
 
-    // Toggle play/pause with space key
+    // Replace playbackRates
     // ---------------------------------
 
     let attr = JSON.parse(player.getAttribute('data-setup'))
     attr.playbackRates = [0.7, 0.8, 1]
     player.setAttribute('data-setup', JSON.stringify(attr))
+
+    // Toggle play/pause with space key
+    // ---------------------------------
+
     document.addEventListener('keydown', (e)=> {
       if(e.keyCode==32) {
         e.preventDefault()
